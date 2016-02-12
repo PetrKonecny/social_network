d267986994eecf5d3938ff3c2d0f1f9006bc4163
Petr <petrknn5@gmail.com>
2016-02-12 16:42:52 +0100

    chat now should stay open on non-nested pages /albums, /groups etc. and new chat window should pop up if user gets a message needs testing and fix bugs with more windows open

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -23,7 +23,6 @@ var ready = function () {
          */
 
         chatWith: function (conversation_id) {
-
             chatBox.createChatBox(conversation_id);
             $("#chatbox_" + conversation_id + " .chatboxtextarea").focus();
         },
@@ -301,7 +300,6 @@ var ready = function () {
             return cookieValue;
         }
     };
-
 
 }
 
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -11,9 +11,10 @@ var ready = function () {
 
         var sender_id = $(this).data('sid');
         var recipient_id = $(this).data('rip');
-
         $.post("/conversations", { sender_id: sender_id, recipient_id: recipient_id }, function (data) {
             chatBox.chatWith(data.conversation_id);
+            conversations.push(new Array(sender_id,recipient_id,data.conversation_id));
+            updateCookie();
         });
     });
 
@@ -23,7 +24,6 @@ var ready = function () {
 
     $(document).on('click', '.toggleChatBox', function (e) {
         e.preventDefault();
-
         var id = $(this).data('cid');
         chatBox.toggleChatBoxGrowth(id);
     });
@@ -34,8 +34,14 @@ var ready = function () {
 
     $(document).on('click', '.closeChat', function (e) {
         e.preventDefault();
-
         var id = $(this).data('cid');
+        for (i=0; i<conversations.length; i++){
+            console.log(conversations[i].toString());
+            if(conversations[i][2] == id){
+                conversations.splice(i,1);
+                updateCookie();
+            }
+        }
         chatBox.close(id);
     });
 
@@ -63,7 +69,69 @@ var ready = function () {
         chatBox.chatWith(conversation_id);
     });
 
+    jQuery.cookie = function (name, value, options) {
+        if (typeof value != 'undefined') { // name and value given, set cookie
+            options = options || {};
+            if (value === null) {
+                value = '';
+                options.expires = -1;
+            }
+            var expires = '';
+            if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+                var date;
+                if (typeof options.expires == 'number') {
+                    date = new Date();
+                    date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+                } else {
+                    date = options.expires;
+                }
+                expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+            }
+            // CAUTION: Needed to parenthesize options.path and options.domain
+            // in the following expressions, otherwise they evaluate to undefined
+            // in the packed version for some reason...
+            var path = options.path ? '; path=' + (options.path) : '';
+            var domain = options.domain ? '; domain=' + (options.domain) : '';
+            var secure = options.secure ? '; secure' : '';
+            document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+        } else { // only name given, get cookie
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+    };
 
+    var conversations = new Array();
+
+
+    if ($.cookie('opened_conversations')) {
+        var fromCookie = JSON.parse($.cookie('opened_conversations'));
+        if (fromCookie.length > 0) {
+            conversations = conversations.concat(fromCookie);
+        }
+        for (i=0; i<conversations.length; i++){
+            console.log(conversations[i].toString());
+            $.post("/conversations", { sender_id: conversations[i][0], recipient_id: conversations[i][1] }, function (data) {
+                chatBox.chatWith(data.conversation_id);
+            });
+        }
+    }else{
+        updateCookie();
+    }
+
+    function updateCookie(){
+        $.cookie('opened_conversations', JSON.stringify(conversations));
+    }
 }
 
 $(document).ready(ready);

f97af0659dedae242d54d0a9c9306d669f37bab4
Jan Mosat <jmosat@gmail.com>
2016-02-14 23:14:40 +0100

    Messages rework, still WIP

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,6 +3,8 @@ class ChatsController < ApplicationController
 
   def show
     @friends = current_user.friends
+    @conversation = params[:format].nil? ? Conversation.first : Conversation.find(params[:format])
     @conversations = Conversation.involving(current_user).select { |x| !x.messages.empty? }.sort { |x, y| y.messages.last.created_at <=> x.messages.last.created_at }
+    @message = Message.new
   end
 end
